a24a14ba48de3b570aa27bb9fd1daaaad117e9a5
zirrilo <163433495+zirrilo@users.noreply.github.com>
2024-09-30 08:06:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'MCLAREM',
+    'Quanto custa uma McLaren P1 GTR?' ,
+    'Há poucos detalhes sobre a dupla, mas fontes que acompanharam a negociação estimam que cada modelo foi adquirido por um valor de R$ 12 a 15 milhões. O hiperesportivo só teve 375 unidades produzidas, sendo a unidade flagrada de número 284.'
+)
+
+criaCartao(
+    
+    'Quantas McLaren P1 GTR tem no mundo?',
+    'A produção estritamente limitada de apenas 375 unidades se esgotou poucos meses depois da abertura das vendas. A entregas começaram em outubro de 2013 e o P1™ se tornou o primeiro supercarro híbrido do mundo.',
+    'MCLAREM'
+)
+
+criaCartao(
+    
+    'Qual a velocidade máxima da McLaren P1?',
+    'O P1 precisa ainda de míseros 6,8 segundos para atingir os 200 km/h e 15,6 segundos para bater os 300 km/h, 0,2 segundos a menos que a Bugatti Veyron SuperSport de 1200 cv. A velocidade máxima está limitada a 350 km/h, a 341 km/h no Porsche 918 Spyder e “mais” de 350 km/h na LaFerrari.',
+    'MCLAREM'
+)
+
+criaCartao(
+    
+    'McLaren F1',
+    'Qual é a McLaren mais cara do mundo? O título de McLaren mais caro vai para o icônico McLaren F1, vendido por US$ 20,5 milhões em um leilão em Pebble Beach, Califórnia, em 2021. Criado por Gordon Murray, com um motor V12 de 6,1 litros da BMW, o McLaren F1 é um dos modelos mais desejados de todos os tempos',
+     'MCLAREM'
+
+    )
